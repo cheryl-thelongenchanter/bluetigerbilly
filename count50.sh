7e5107e085cba28e1f54a130de180c388c45a005
cheryl-thelongenchanter <mike.bd.clark@gmail.com>
2024-12-04 20:33:19 -0700

    MC 12/4/24

--- count50.sh
+++ count50.sh
@@ -1,3 +1,10 @@
+# Michael Clark
+# 12/4/2024
+# CIS20231N1 Linux, Professor Gilson
+# The script uses a for loop to iterate a generated sequence on numbers from
+# 1 to 50 and prints to the standard output. The script exits once it
+# reaches the end of the sequence which is 50
+
 for i in $(seq 50)
 	do
 		echo "$i"
